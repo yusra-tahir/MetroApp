b2e7f89f20b846f2c5ecaa4a9efd62f0bcd200ee
Yusra Tahir <yusra-tahir@hotmail.co.uk>
2023-01-19 19:01:56 +0000

    user-service

---
+++ user-service/src/test/java/com/userService/client/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userService.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
